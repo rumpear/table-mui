05fd4e36285c96312eae460f33f0f8239260468d
rumpear <roman.f.grusha@gmail.com>
2022-07-06 17:45:11 +0300

    add pages

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import { EnhancedTable } from "../../components/EnhancedTable";
+import "./styles.scss";
+
+const MainPage = () => {
+  return (
+    <>
+      <h1 className="MainPage-title">Welcome to the main page</h1>
+      <br></br>
+      <br></br>
+      <EnhancedTable />
+    </>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { MainPage } from './MainPage';
+export { TablePage } from './TablePage';
