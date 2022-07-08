01807fe191c165df9d5f7c0b38cd709c91fab122
rumpear <roman.f.grusha@gmail.com>
2022-07-08 18:30:55 +0300

    BasicInput added

---
+++ src/components/ui/BasicInput/BasicInput.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { useStyles } from './styles';
+
+const BasicInput = ({
+  id,
+  name,
+  label,
+  value,
+  error,
+  helperText,
+  placeholder,
+  onChange,
+}) => {
+  const styles = useStyles();
+
+  const handleChange = e => {
+    const { value } = e.target;
+    onChange(value);
+  };
+
+  return (
+    <FormControl error={error} className={styles.formControl}>
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <OutlinedInput
+        id={id}
+        name={name}
+        label={label}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+      />
+      {Boolean(helperText) && (
+        <FormHelperText id={id}>{helperText}</FormHelperText>
+      )}
+    </FormControl>
+  );
+};
+
+export default BasicInput;
+
+BasicInput.defaultProps = {
+  name: '',
+  label: 'input',
+  error: false,
+};
+
+BasicInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ src/components/ui/BasicInput/styles.js
@@ -0,0 +1,8 @@
+import makeStyles from '@mui/styles/makeStyles';
+
+export const useStyles = makeStyles({
+  formControl: {
+    width: '100%',
+    margin: '10px 0',
+  },
+});
