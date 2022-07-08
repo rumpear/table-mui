7956f73cb62ee720fc8c83d6ea8aff36318726f5
rumpear <roman.f.grusha@gmail.com>
2022-07-08 14:38:01 +0300

    added jssPreset

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
+import {
+  createGenerateClassName,
+  jssPreset,
+  StylesProvider,
+  ThemeProvider,
+} from '@mui/styles';
 import { Route, Routes } from 'react-router-dom';
 import { MainPage, TablePage } from '../pages/';
+import jssIncreaseSpecificity from 'jss-increase-specificity';
+import { create } from 'jss';
 import { Layout } from './';
+import { createTheme } from '@mui/material';
+
+const jss = create({
+  plugins: [...jssPreset().plugins, jssIncreaseSpecificity()],
+});
+
+const theme = createTheme({
+  // palette: {
+  //   background: { paper: 'red' },
+  // },
+});
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'romanGrusha-',
+  disableGlobal: false,
+  seed: 'rg',
+});
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path="/table" element={<TablePage />} />
-      </Route>
-    </Routes>
+    <StylesProvider jss={jss} generateClassName={generateClassName}>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/table" element={<TablePage />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </StylesProvider>
   );
 };
 
