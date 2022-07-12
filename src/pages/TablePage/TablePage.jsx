90c6b33dcfd908c07571e9fc8ba566cedb567927
rumpear <roman.f.grusha@gmail.com>
2022-07-12 15:54:32 +0300

    upd App

--- src/pages/TablePage/TablePage.jsx
+++ src/pages/TablePage/TablePage.jsx
@@ -1,46 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
-import { getData } from '../../services/tableApi';
-import { Table } from '../../components/';
 import { EnhancedTable } from '../../components/';
 
 import './styles.scss';
 
 const TablePage = () => {
-  const [tableData, setTableData] = useState([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const data = await getData();
-      setTableData(data);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
   return (
     <>
       <h1 className="TablePage-title">Table page</h1>
-      {Boolean(error) && <p className="TablePage-error">{error}</p>}
 
-      {isLoading ? (
-        <p className="TablePage-loading">Loading...</p>
-      ) : (
-        Boolean(tableData.length) && <EnhancedTable rows={tableData} />
-      )}
-
-      {/* {isLoading ? (
-        <p className="TablePage-loading">Loading...</p>
-      ) : (
-        Boolean(tableData.length) && <Table data={tableData} />
-      )} */}
+      <EnhancedTable />
     </>
   );
 };
